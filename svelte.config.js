553dfd0f71f38b34e46d8a714f01f1fb7e9b299e
Camille Okubo <camille.okubo.etu@univ-lille.fr>
2024-12-20 01:40:33 +0100

    a

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
-const dev = process.env.NODE_ENV === 'development';
-
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
-		adapter: adapter(),
-		paths: {
-			base: dev ? '' : '/theaereus.github.io/',
-		}
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
+
+export default config;
